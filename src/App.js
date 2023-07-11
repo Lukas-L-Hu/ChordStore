e2cd2cfb239367965ab0ca183ec118c5de7b5fa6
Lukas Hu <lukashu22@gmail.com>
2023-07-10 21:17:57 -0700

    Decreased size of the save button

--- src/App.js
+++ src/App.js
@@ -7,18 +7,7 @@ import AddTask from './components/add_task'
 
 function App() {
   const [showAddTask, setShowAddTask] = useState (false)
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Task one",
-      link: ''
-    },
-    {
-      id: 2,
-      text: "Task two",
-      link: ''
-    }
-  ])
+  const [tasks, setTasks] = useState([])
 
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 1000) + 1
